088b8c6300fac14eabbddc1a9160e4c24a8f7008
RakhiGarhwal <garwalrakhi@gmail.com>
2026-05-18 11:17:36 +0530

    serve frontend

--- server/index.js
+++ server/index.js
@@ -2,34 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
-
-const employeeRoutes = require("./routes/employeeRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", employeeRoutes);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"));
+app.use("/api/employees", require("./routes/employeeRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
-});
-
-const path = require("path");
-
+// FRONTEND SERVE
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get(/.*/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server started");
 });
